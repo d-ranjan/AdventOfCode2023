81115fda3eed1ffcde491ac1af13a9f52245949b
Dipu Ranjan <dipuranjanec08@gmail.com>
2024-12-05 19:44:38 +0530

    day 0 part1 and part 2 templates

---
+++ day00/part1.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <algorithm>
+#include <fmt/ranges.h>
+#include <ranges>
+#include <string_view>
+
+namespace part_one
+{
+
+[[nodiscard]] constexpr static auto get_result(const std::string_view document) -> int
+{
+    // Accumulate the values
+    return std::ranges::fold_left(document | std::views::split('\n'), 0,
+                                  [&](int sum, auto&& line) {
+        fmt::println("{}", std::string_view {line});
+        return sum;
+    });
+}
+
+} // namespace part_one
